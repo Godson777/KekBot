ccffd604dee8248994479a39137a69da081c332b
Godson <Godson777@users.noreply.github.com>
2016-10-22 19:18:02 -0400

    Updated the version of the modded Commands4J KekBot uses, which gives meme commands a 1-in-20 chance to fail.

--- src/main/java/com/godson/kekbot/command/commands/ping.java
+++ src/main/java/com/godson/kekbot/command/commands/ping.java
@@ -6,6 +6,8 @@ import com.darichey.discord.api.CommandCategory;
 import com.godson.kekbot.EasyMessage;
 import sx.blah.discord.handle.obj.IMessage;
 
+import java.util.Random;
+
 public class ping {
     public static Command test = new Command("test")
             .withCategory(CommandCategory.TEST)
@@ -13,8 +15,12 @@ public class ping {
             .withUsage("{p}test")
             .caseSensitive(true)
             .onExecuted(context -> {
-                if (!context.getMessage().getAttachments().isEmpty()) {
-
+                Random random = new Random();
+                int number = random.nextInt(20);
+                if (number < 19) {
+                    EasyMessage.send(context.getMessage().getChannel(), "k");
+                } else {
+                    EasyMessage.send(context.getMessage().getChannel(), "no u");
                 }
             });
 }
