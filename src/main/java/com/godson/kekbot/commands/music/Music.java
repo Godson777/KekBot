1a52ee75f0418d27fae06f5b362d5de01f025cf9
Godson <Godson777@users.noreply.github.com>
2016-12-13 02:24:13 -0500

    Added $music, which prints out a list of all music commands (if there's any music playing.)

---
+++ src/main/java/com/godson/kekbot/commands/music/Music.java
@@ -0,0 +1,39 @@
+package com.godson.kekbot.commands.music;
+
+import com.darichey.discord.api.Command;
+import com.godson.kekbot.KekBot;
+import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.VoiceChannel;
+
+import java.util.Optional;
+
+public class Music {
+    public static Command music = new Command("music")
+            .withDescription("")
+            .withUsage("{p}music")
+            .onExecuted(context -> {
+                Optional<VoiceChannel> voiceChannel = context.getGuild().getVoiceChannels().stream().filter(c -> c.getMembers().contains(context.getMember())).findFirst();
+                if (!voiceChannel.isPresent()) {
+                    context.getTextChannel().sendMessage("This command requies you to be in a voice channel!").queue();
+                } else {
+                    if (context.getGuild().getAudioManager().isConnected()) {
+                        if (KekBot.player.isMeme(context.getGuild())) {
+                            context.getTextChannel().sendMessage("You cannot call music commands while memes are playing.").queue();
+                        } else {
+                            if (context.getGuild().getAudioManager().getConnectedChannel().equals(voiceChannel.get())) {
+                                context.getTextChannel().sendMessage(KekBot.replacePrefix(context.getGuild(), "Music Commands: " +
+                                        "\n{p}queue - **Queues a music track.**" +
+                                        "\n{p}skip - **Skips a track. (Host Only)**" +
+                                        "\n{p}song - **Gets the current song info.**" +
+                                        "\n{p}playlist - **Lists all the tracks that are in the queue.**" +
+                                        "\n{p}volume - **Sets the volume. (Host Only)**" +
+                                        "\n{p}host - **Makes someone else the \"Host\". (Host Only)**" +
+                                        "\n{p}stop - **Stops the current music session. (Host Only)**" +
+                                        "\n" +
+                                        "\nAll \"Host Only\" commands can also be executed by a user with `Administrator` permissions.")).queue();
+                            } else context.getTextChannel().sendMessage("You have to be in \"" + context.getGuild().getAudioManager().getConnectedChannel().getName() + "\" in order to use music commands.").queue();
+                        }
+                    } else context.getTextChannel().sendMessage("I'm not even playing any music!").queue();
+                }
+            });
+}
