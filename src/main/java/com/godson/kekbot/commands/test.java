aaad9e2cd69890ecd750409165b2729b153221ff
Godson <Godson777@users.noreply.github.com>
2017-01-05 02:03:18 -0500

    New owner command. Might have to consider giving these descriptions and usages for reference in the help command...

--- src/main/java/com/godson/kekbot/commands/test.java
+++ src/main/java/com/godson/kekbot/commands/test.java
@@ -6,6 +6,12 @@ import com.godson.kekbot.KekBot;
 import com.google.gson.Gson;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.Member;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class test {
     public static Command test = new Command("test")
