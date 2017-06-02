fb84a5843a2c5124e9f37a4e3407a84b3b62acba
Godson <Godson777@users.noreply.github.com>
2017-06-02 06:29:17 -0400

    test

--- src/main/java/com/godson/kekbot/commands/test.java
+++ src/main/java/com/godson/kekbot/commands/test.java
@@ -1,7 +1,35 @@
 package com.godson.kekbot.commands;
 
 import com.darichey.discord.api.Command;
+import com.darichey.discord.api.CommandRegistry;
+import com.godson.kekbot.GSONUtils;
 import com.godson.kekbot.KekBot;
+import com.godson.kekbot.Listener;
+import com.godson.kekbot.Music.Playlist;
+import com.godson.kekbot.Profile.Badge;
+import com.godson.kekbot.Profile.Profile;
+import com.godson.kekbot.Profile.Token;
+import com.godson.kekbot.commands.admin.*;
+import com.godson.kekbot.commands.fun.*;
+import com.godson.kekbot.commands.general.*;
+import com.godson.kekbot.commands.meme.*;
+import com.godson.kekbot.commands.music.*;
+import com.godson.kekbot.commands.owner.*;
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.VoiceChannel;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+
+import javax.imageio.ImageIO;
+import javax.security.auth.login.LoginException;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Optional;
 
 public class test {
     public static Command test = new Command("test")
