af773284d7f52291315256c24377fb3de6ab971b
Godson <Godson777@users.noreply.github.com>
2017-07-02 06:29:59 -0400

    `pick` command added.

---
+++ src/main/java/com/godson/kekbot/commands/fun/Pick.java
@@ -0,0 +1,40 @@
+package com.godson.kekbot.commands.fun;
+
+import com.darichey.discord.api.Command;
+import com.darichey.discord.api.CommandCategory;
+import com.godson.kekbot.KekBot;
+import com.godson.kekbot.Responses.Action;
+import net.dv8tion.jda.core.entities.TextChannel;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class Pick {
+    public static Command pick = new Command("pick")
+            .withAliases("choose", "decide")
+            .withCategory(CommandCategory.FUN)
+            .withDescription("Has KekBot pick ")
+            .withUsage("{p}pick <option> | <option> {can continue adding more options by seperating them with | }")
+            .onExecuted(context -> {
+                String rawSplit[] = context.getMessage().getRawContent().split(" ", 2);
+                TextChannel channel = context.getTextChannel();
+                if (rawSplit.length > 1) {
+                    List<String> toFormat = Arrays.asList(rawSplit[1].split("\\u007c"));
+                    List<String> choices = new ArrayList<>();
+                    for (String string : toFormat) {
+                        String choice = KekBot.removeWhitespaceEdges(string);
+                        if (!choice.equals("")) choices.add(choice);
+                    }
+                    if (choices.size() > 1) {
+                        Random random = new Random();
+                        channel.sendMessage(KekBot.respond(context, Action.CHOICE_MADE, choices.get(random.nextInt(choices.size())))).queue();
+                    } else {
+                        channel.sendMessage("Well, I guess I'm choosing `" + choices.get(0) + "`, since you haven't given me anything else to pick...").queue();
+                    }
+                } else {
+                    channel.sendMessage("You haven't given me any choices, though...").queue();
+                }
+            });
+}
