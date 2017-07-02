5bab802dc44b3bf39df072eb08fe6232774582ab
Godson <Godson777@users.noreply.github.com>
2017-07-02 06:23:05 -0400

    Shard tools are a nice thing to have.

--- src/main/java/com/godson/kekbot/Utils.java
+++ src/main/java/com/godson/kekbot/Utils.java
@@ -4,12 +4,15 @@ import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.entities.User;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Utils {
     public static boolean deleteDirectory(File directory) {
-        if(directory.exists()){
+        if (directory.exists()){
             File[] files = directory.listFiles();
-            if(null!=files){
+            if (null != files){
                 for (File file : files) {
                     if (file.isDirectory()) {
                         deleteDirectory(file);
@@ -20,5 +23,15 @@ public class Utils {
             }
         }
         return(directory.delete());
+    }
+
+    public static User findUser(String userId) {
+        List<User> users = new ArrayList<>();
+        for (int i = 0; i < KekBot.jdas.length; i++) {
+            users.addAll(KekBot.jdas[i].getUsers().stream().filter(user -> !users.contains(user)).collect(Collectors.toList()));
+        }
+        if (users.stream().anyMatch(user -> user.getId().equals(userId))) {
+            return users.stream().filter(user -> user.getId().equals(userId)).findAny().get();
+        } else throw new NullPointerException();
     }
 }
