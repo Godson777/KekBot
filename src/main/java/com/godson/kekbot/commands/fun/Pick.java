ad03a82cb39b1ff98159d53c6cfe06292eb386d7
Hutch Moore <tech_hutch@verizon.net>
2017-07-12 11:52:59 -0400

    Fixed bug with pick command

--- src/main/java/com/godson/kekbot/commands/fun/Pick.java
+++ src/main/java/com/godson/kekbot/commands/fun/Pick.java
@@ -18,23 +18,28 @@ public class Pick {
             .withDescription("Has KekBot pick ")
             .withUsage("{p}pick <option> | <option> {can continue adding more options by seperating them with | }")
             .onExecuted(context -> {
+                String noChoicesGiven = "You haven't given me any choices, though...";
+
                 String rawSplit[] = context.getMessage().getRawContent().split(" ", 2);
                 TextChannel channel = context.getTextChannel();
-                if (rawSplit.length > 1) {
-                    List<String> toFormat = Arrays.asList(rawSplit[1].split("\\u007c"));
-                    List<String> choices = new ArrayList<>();
-                    for (String string : toFormat) {
-                        String choice = KekBot.removeWhitespaceEdges(string);
-                        if (!choice.equals("")) choices.add(choice);
-                    }
-                    if (choices.size() > 1) {
-                        Random random = new Random();
-                        channel.sendMessage(KekBot.respond(context, Action.CHOICE_MADE, choices.get(random.nextInt(choices.size())))).queue();
-                    } else {
-                        channel.sendMessage("Well, I guess I'm choosing `" + choices.get(0) + "`, since you haven't given me anything else to pick...").queue();
-                    }
+                if (rawSplit.length <= 1) {
+                    channel.sendMessage(noChoicesGiven).queue();
+                    return;
+                }
+
+                List<String> toFormat = Arrays.asList(rawSplit[1].split("\\u007c"));
+                List<String> choices = new ArrayList<>();
+                for (String string : toFormat) {
+                    String choice = KekBot.removeWhitespaceEdges(string);
+                    if (!choice.equals("")) choices.add(choice);
+                }
+                if (choices.size() > 1) {
+                    Random random = new Random();
+                    channel.sendMessage(KekBot.respond(context, Action.CHOICE_MADE, choices.get(random.nextInt(choices.size())))).queue();
+                } else if (choices.size() == 1) {
+                    channel.sendMessage("Well, I guess I'm choosing `" + choices.get(0) + "`, since you haven't given me anything else to pick...").queue();
                 } else {
-                    channel.sendMessage("You haven't given me any choices, though...").queue();
+                    channel.sendMessage(noChoicesGiven).queue();
                 }
             });
 }
