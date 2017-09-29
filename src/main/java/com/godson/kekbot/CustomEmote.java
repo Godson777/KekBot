201759025da32233f269e495ac46b4b7dd8ddf06
Godson <Godson777@users.noreply.github.com>
2017-09-29 04:37:25 -0400

    CustomEmote now has a method that makes printing amounts of topkeks more convenient.

--- src/main/java/com/godson/kekbot/CustomEmote.java
+++ src/main/java/com/godson/kekbot/CustomEmote.java
@@ -3,17 +3,26 @@ package com.godson.kekbot;
 import java.util.Random;
 
 public class CustomEmote {
-    private static String[] THINKINGS =
+    private final static String[] THINKINGS =
             {"<:GRANDTHONK:283540692502970368>",
             "<:Thunk:280126422213853194>",
             "<:thinkingnohands:319056770943287296>",
             "<:OwOThink:319056770959933440>",
             "<:thonkang:319056771027304448>",
             "\uD83E\uDD14"};
-    public static String TOPKEK = "<:topkek:317825573441503243>";
+    private final static String TOPKEK = "<:topkek:317825573441503243>";
     private static Random random = new Random();
 
     public static String think() {
         return THINKINGS[random.nextInt(THINKINGS.length)];
+    }
+
+    public static String printPrice(double price) {
+        if (price % 1 == 0) return (int) price + TOPKEK;
+        else return price + TOPKEK;
+    }
+
+    public static String printTopKek() {
+        return TOPKEK;
     }
 }
