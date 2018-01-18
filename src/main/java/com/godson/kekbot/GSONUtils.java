df5590cbe229d565c0e28cdecd0db919f8db478d
Godson <Godson777@users.noreply.github.com>
2018-01-18 04:44:00 -0500

    old and unneeded methods are gone now

--- src/main/java/com/godson/kekbot/GSONUtils.java
+++ src/main/java/com/godson/kekbot/GSONUtils.java
@@ -1,10 +1,10 @@
 package com.godson.kekbot;
 
-import com.godson.kekbot.Objects.UDictionary;
-import com.godson.kekbot.Responses.Action;
-import com.godson.kekbot.Responses.Responder;
-import com.godson.kekbot.Responses.ResponseSuggestions;
-import com.godson.kekbot.Settings.*;
+import com.godson.kekbot.objects.UDictionary;
+import com.godson.kekbot.responses.Action;
+import com.godson.kekbot.responses.Responder;
+import com.godson.kekbot.responses.ResponseSuggestions;
+import com.godson.kekbot.settings.*;
 import com.google.gson.Gson;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
@@ -17,113 +17,6 @@ import java.util.List;
 import java.util.Optional;
 
 public class GSONUtils {
-
-    public static Settings getSettings(Guild guild) {
-        Settings settings = null;
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("settings/" + guild.getId() + "/Settings.json"));
-            Gson gson = new Gson();
-            settings = gson.fromJson(br, Settings.class);
-            br.close();
-        } catch (FileNotFoundException e) {
-            settings = new Settings();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return settings;
-    }
-
-    public static Responder getResponder(Action action) {
-        Responder response = new Responder(action);
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("responses/" + action.name() + ".json"));
-            Gson gson = new Gson();
-            response = gson.fromJson(br, Responder.class);
-            response.setAction(action);
-            br.close();
-        } catch (FileNotFoundException e) {
-            //do nothing
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return response;
-    }
-
-    public static ResponseSuggestions getSuggestions() {
-        ResponseSuggestions suggestions = new ResponseSuggestions();
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("responses/responses.json"));
-            Gson gson = new Gson();
-            suggestions = gson.fromJson(br, ResponseSuggestions.class);
-            br.close();
-        } catch (FileNotFoundException e) {
-            //do nothing
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return suggestions;
-    }
-
-    public static TagManager getTagManager(Guild guild) {
-        TagManager manager = new TagManager();
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("settings/" + guild.getId() + "/Tags.json"));
-            Gson gson = new Gson();
-            manager = gson.fromJson(br, TagManager.class);
-            br.close();
-        } catch (FileNotFoundException e) {
-            //do nothing
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return manager;
-    }
-
-    public static Quotes getQuotes(Guild guild) {
-        Quotes quotes = new Quotes();
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("settings/" + guild.getId() + "/Quotes.json"));
-            Gson gson = new Gson();
-            quotes = gson.fromJson(br, Quotes.class);
-            br.close();
-        } catch (FileNotFoundException e) {
-            //do nothing
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return quotes;
-    }
-
-    public static Config getConfig() {
-        Config config = new Config();
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("config/config.json"));
-            Gson gson = new Gson();
-            config = gson.fromJson(br, Config.class);
-            br.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("config.json not found! What have you done with it?!");
-            System.exit(0);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return config;
-    }
-
-    public static TicketManager getTicketManager() {
-        TicketManager manager = new TicketManager();
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("tickets.json"));
-            Gson gson = new Gson();
-            manager = gson.fromJson(br, TicketManager.class);
-            br.close();
-        } catch (FileNotFoundException e) {
-            manager.save();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return manager;
-    }
 
     public static UDictionary getUDResults(String word) {
         UDictionary uDictionary = new UDictionary();
