7b6c4b9b483459e9a0d8b35a6f417e99ac3ee270
Godson <Godson777@users.noreply.github.com>
2018-02-28 04:33:30 -0500

    Now you can check others balances.

--- src/main/java/com/godson/kekbot/command/commands/fun/Balance.java
+++ src/main/java/com/godson/kekbot/command/commands/fun/Balance.java
@@ -19,6 +19,16 @@ public class Balance extends Command {
 
     @Override
     public void onExecuted(CommandEvent event) {
+        if (event.getArgs().length > 0) {
+            if (event.getMessage().getMentionedUsers().size() != 1) {
+                event.getChannel().sendMessage((event.getMessage().getMentionedUsers().size() > 1 ? "Too many" : "No") + "users specified.").queue();
+                return;
+            }
+            Profile profile = Profile.getProfile(event.getMessage().getMentionedUsers().get(0));
+            event.getChannel().sendMessage(event.getMessage().getMentionedUsers().get(0).getName() + " has " + CustomEmote.printPrice(profile.getTopkeks())).queue();
+            return;
+        }
+
         Profile profile = Profile.getProfile(event.getAuthor());
         event.getChannel().sendMessage("You have " + CustomEmote.printPrice(profile.getTopkeks())).queue();
     }
