f36baf7c74c7e3d37f85c51b7967eaae8fe7db89
Godson <Godson777@users.noreply.github.com>
2018-02-28 04:24:47 -0500

    I NEEEEEED THIS

---
+++ src/main/java/com/godson/kekbot/TriConsumer.java
@@ -0,0 +1,18 @@
+package com.godson.kekbot;
+
+import java.util.Objects;
+
+@FunctionalInterface
+public interface TriConsumer<T, U, V> {
+
+    void accept(T t, U u, V v);
+
+    default TriConsumer<T, U, V> andThen(TriConsumer<? super T, ? super U, ? super V> after) {
+        Objects.requireNonNull(after);
+
+        return (t, u, v) -> {
+            accept(t, u, v);
+            after.accept(t, u, v);
+        };
+    }
+}
