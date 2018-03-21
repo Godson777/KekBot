c725fbbd2e5ce527d08952082a3ea20f63c0c71f
Hutch <tech_hutch@verizon.net>
2018-03-21 19:35:09 -0400

    Prett much finished the Tag and Possible classes, & added new Throwable
    * Finished abstracting the Possible class
    * Added some documentation to Tag and Possible
    * Added a new class, ThrowableString, as an easy way to bubble up error
      messages when you just want to get a string out there (and cough cough
      because it lets me have vestiges of JS, where you can throw anything)

--- src/main/java/com/godson/kekbot/command/usage/Possible.java
+++ src/main/java/com/godson/kekbot/command/usage/Possible.java
@@ -2,36 +2,51 @@ package com.godson.kekbot.command.usage;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 
+import com.godson.kekbot.ThrowableString;
 import com.godson.kekbot.Utils;
 
 /**
- * Represents a possibility in a usage Tag
+ * Represents a possible type/value in a usage Tag
  */
 public abstract class Possible {
 
-    protected static final Map<String, Class<? extends Number>> minMaxTypeClasses = new HashMap<>(3);
+    /**
+     * A map of Possible types (ones that support min/max values) to the type their min/max values must have
+     */
+    protected static final Map<String, String> minMaxTypeClasses = new HashMap<>(3);
     static {
-        minMaxTypeClasses.put("string", Integer.class);
-        minMaxTypeClasses.put("int", Integer.class);
-        minMaxTypeClasses.put("float", Float.class);
+        minMaxTypeClasses.put("String", "int");
+        minMaxTypeClasses.put("int", "int");
+        minMaxTypeClasses.put("float", "float");
     }
+
+    /**
+     * The Possible types that can have min/max values
+     */
     protected static final Set<String> minMaxTypes = minMaxTypeClasses.keySet();
-    protected static final List<String> regexTypes = Arrays.asList("pattern", "regex");
 
+    /**
+     * The Possible types that can have regular expressions
+     */
+    protected static final List<String> regexTypes = Arrays.asList("Pattern", "regex");
+
+    /**
+     * The name of this Possible
+     */
     public final String name;
-    public final String type;
-    public final Number min;
-    public final Number max;
-    public final Pattern regex;
 
-    protected Possible(String[] parts) {
+    /**
+     * The type of this Possible
+     */
+    public final String type;
+
+    protected Possible(String[] parts) throws ThrowableString {
         assert parts.length == 6;
 
         name = parts[1];
@@ -40,73 +55,126 @@ public abstract class Possible {
         type = parts[2] == null ? "literal" : parts[2].toLowerCase();
         assert type != null;
 
-        final boolean canHaveMinMax = minMaxTypes.contains(type);
-        if (parts[3] == null) {
-            min = null;
-        } else {
-            if (canHaveMinMax) min = resolveLimit(parts[3], type, "min");
-            else throw new IllegalArgumentException("Type " + type + " cannot have a minimum value");
-        }
-        if (parts[4] == null) {
-            max = null;
-        } else {
-            if (canHaveMinMax) max = resolveLimit(parts[4], type, "max");
-            else throw new IllegalArgumentException("Type " + type + " cannot have a maximum value");
+        if (!minMaxTypes.contains(type)) {
+            if (parts[3] != null) throw new ThrowableString("Type " + type + " cannot have a minimum value");
+            if (parts[4] != null) throw new ThrowableString("Type " + type + " cannot have a maximum value");
         }
 
-        if (regexTypes.contains(type)) {
-            if (parts[5] == null) throw new IllegalArgumentException("Regex types must include a regular expression");
-            if (parts[6] == null) regex = Pattern.compile(parts[5]);
-            else regex = Pattern.compile(parts[5], Utils.resolvePatternFlags(parts[6]));
-        } else {
-            if (parts[5] != null || parts[6] != null) throw new IllegalArgumentException("Type " + type + " is not a regex type, yet a regular expression was provided");
-            regex = null;
+        if (!regexTypes.contains(type)) {
+            if (parts[5] != null) throw new ThrowableString("Type " + type + " is not a regex type, yet a regular expression was provided");
+            if (parts[6] != null) throw new ThrowableString("Type " + type + " is not a regex type, yet regular expression flags were provided");
         }
     }
 
-    public static Possible makeFromMatchResult(MatchResult regexResult) {
+    /**
+     * Make a Possible, of the appropriate type
+     */
+    public static Possible makeFromMatchResult(MatchResult regexResult) throws ThrowableString {
         assert regexResult.groupCount() == 6;
 
         final String[] parts = new String[6];
         for (int i = 1; i <= 6; i++) parts[i] = regexResult.group(i);
-        final String typeOrNull = parts[2];
 
+        final String typeOrNull = parts[2];
         if (minMaxTypes.contains(typeOrNull)) {
-            switch (minMaxTypeClasses.get(typeOrNull).getSimpleName()) {
-                case "Integer": return new StringOrIntPossible(parts);
+            switch (minMaxTypeClasses.get(typeOrNull)) {
+                case "int": return new StringOrIntPossible(parts);
+                case "float": return new FloatPossible(parts);
                 default: assert false : "Forgot to implement this min and max type";
             }
+        } else if (regexTypes.contains(typeOrNull)) {
+            return new RegexPossible(parts);
         }
         return new OtherPossible(parts);
     }
 
-    private static class OtherPossible extends Possible {
-        public OtherPossible(String[] parts) { super(parts); }
+    /**
+     * Represents a possible type/value in a usage Tag
+     */
+    public static class OtherPossible extends Possible {
+        protected OtherPossible(String[] parts) throws ThrowableString {
+            super(parts);
+        }
     }
 
-    private static class StringOrIntPossible extends Possible {
-        public StringOrIntPossible(String[] parts) {
+    /**
+     * Represents a possible String/int type/value in a usage Tag
+     */
+    public static class StringOrIntPossible extends Possible {
+        /**
+         * The minimum value this Possible allows
+         */
+        public final int min;
+
+        /**
+         * The maximum value this Possible allows
+         */
+        public final int max;
+
+        protected StringOrIntPossible(String[] parts) throws ThrowableString {
             super(parts);
-            // TODO: int checking
-            // final boolean canHaveMinMax = minMaxTypes.contains(type);
-            // min = parts[3] == null ? null : resolveLimit(parts[3], type, "min");
-            // if (parts[4] == null) {
-            //     max = null;
-            // } else {
-            //     if (canHaveMinMax) max = resolveLimit(parts[4], type, "max");
-            //     else throw new IllegalArgumentException("Type " + type + " cannot have a maximum value");
-            // }
+            assert minMaxTypes.contains(type);
+            min = parts[3] == null ? null : resolveLimit(parts[3], "min");
+            max = parts[4] == null ? null : resolveLimit(parts[4], "max");
         }
 
-        private static Number resolveLimit(String limit, String type, String limitType) {
-            Integer tempLimit;
+        private static int resolveLimit(String limit, String limitType) throws ThrowableString {
+            int tempLimit;
             try {
-                tempLimit = Integer.valueOf(limit);
+                tempLimit = Integer.parseInt(limit);
             } catch (NumberFormatException e) {
-                throw new NumberFormatException(limitType + " must be an integer for this type.");
-                //throw new NumberFormatException(limitType + " must be an integer");
+                throw (ThrowableString) new ThrowableString(limitType + " must be an integer for this type, if provided.", e);
             }
             return tempLimit;
+        }
+    }
+
+    /**
+     * Represents a possible float type/value in a usage Tag
+     */
+    public static class FloatPossible extends Possible {
+        /**
+         * The minimum value this Possible allows
+         */
+        public final float min;
+
+        /**
+         * The maximum value this Possible allows
+         */
+        public final float max;
+
+        protected FloatPossible(String[] parts) throws ThrowableString {
+            super(parts);
+            assert minMaxTypes.contains(type);
+            min = parts[3] == null ? null : resolveLimit(parts[3], "min");
+            max = parts[4] == null ? null : resolveLimit(parts[4], "max");
+        }
+
+        private static float resolveLimit(String limit, String limitType) throws ThrowableString {
+            float tempLimit;
+            try {
+                tempLimit = Float.parseFloat(limit);
+            } catch (NumberFormatException e) {
+                throw (ThrowableString) new ThrowableString(limitType + " must be a float for this type, if provided.", e);
+            }
+            return tempLimit;
+        }
+    }
+
+    /**
+     * Represents a possible Pattern type/value in a usage Tag
+     */
+    public static class RegexPossible extends Possible {
+        /**
+         * The regular expression this Possible matches against
+         */
+        public final Pattern regex;
+
+        protected RegexPossible(String[] parts) throws ThrowableString {
+            super(parts);
+            if (parts[5] == null) throw new ThrowableString("Regex types must include a regular expression");
+            if (parts[6] == null) regex = Pattern.compile(parts[5]);
+            else regex = Pattern.compile(parts[5], Utils.resolvePatternFlags(parts[6]));
         }
     }
 }
--- src/main/java/com/godson/kekbot/command/usage/Tag.java
+++ src/main/java/com/godson/kekbot/command/usage/Tag.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+
+import com.godson.kekbot.ThrowableString;
 
 /**
  * Represents one tag, one argument, in a usage
@@ -15,54 +15,77 @@ public class Tag {
     /**
      * Matches usage tags
      */
-    private static Pattern pattern = Pattern.compile("^([^:]+)(?::([^{}/]+))?(?:{([^,]+)?(?:,(.+))?})?(?:\\/(.+)\\/(\\w+)?)?$", Pattern.CASE_INSENSITIVE);
+    private static final Pattern pattern = Pattern.compile("^([^:]+)(?::([^{}/]+))?(?:{([^,]+)?(?:,(.+))?})?(?:\\/(.+)\\/(\\w+)?)?$", Pattern.CASE_INSENSITIVE);
+
+    public static enum Required {
+        OPTIONAL, SEMI_REQUIRED, REQUIRED
+    }
 
     /**
-     * The type of tag (0 for optional, anything else for required)
+     * The type of tag (0 optional, 1 semi-required, 2 required)
      */
-    public int required;
+    public final Required required;
     
     /**
      * The "possibilities" of this tag
-     * TODO typing
      */
-    public Object[] possibles;
+    public final Possible[] possibles;
 
     /**
      * Whether this tag is repeating
+     *
+     * This is set by the Usage instance that contains this Tag.
      */
     public boolean repeat = false;
 
-    public Tag(String contents, int position, int required) {
+    public Tag(String contents, int position, Required required) throws ThrowableString {
         this.required = required;
         this.possibles = Tag.parseContents(contents, position);
     }
 
-    private static Possible[] parseContents(String contents, int position) {
+    /**
+     * Parse the contents of a Tag into usable Possibles
+     */
+    private static Possible[] parseContents(String contents, int position) throws ThrowableString {
         final List<String> literals = new ArrayList<>();
         final List<String> types = new ArrayList<>();
-        final List<String> tags = Tag._parseRawContents(contents);
-        return IntStream.range(0, tags.size()).mapToObj(i -> {
-            final String tag = tags.get(i);
+        final List<String> rawPossibles = Tag._parseRawContents(contents);
+
+        final Possible[] possibles = new Possible[rawPossibles.size()];
+        final boolean moreThanOnePossible = rawPossibles.size() > 1;
+        final int lastIndex = rawPossibles.size() - 1;
+        for (int i = 0; i < rawPossibles.size(); i++) {
+            final String tag = rawPossibles.get(i);
             final String current = String.format(
                 "%s: at tag #%d at bound #%d",
-                tags, position, i + 1
+                rawPossibles, position, i + 1
             );
             Possible possible;
+            final Matcher matcher = Tag.pattern.matcher(tag);
+            matcher.find();
             try {
-                final Matcher matcher = Tag.pattern.matcher(tag);
-                matcher.find();
-                possible = new Possible(matcher.toMatchResult());
-            } catch (Exception e) {
-                //TODO: handle exception
+                possible = Possible.makeFromMatchResult(matcher.toMatchResult());
+            } catch (ThrowableString e) {
+                throw new ThrowableString(current + ": " + e.getMessage(), e);
             }
-            // TODO
-            return possible;
-        }).toArray(Possible[]::new);
+            if (possible.type.equals("literal")) {
+                if (literals.contains(possible.name)) throw new ThrowableString(current + ": there can't be two literals with the same text.");
+                literals.add(possible.name);
+            } else if (moreThanOnePossible) {
+                if (i != lastIndex && possible.type.equals("String")) throw new ThrowableString(current + ": the String type is vague, you must specify it as the last type");
+                if (types.contains(possible.type)) throw new ThrowableString(current + ": there can't be two possibles with the same type (" + possible.type + ")");
+                types.add(possible.type);
+            }
+            possibles[i] = possible;
+        }
+        return possibles;
     }
 
+    /**
+     * Parse the contents of a Tag into strings representing each Possible
+     */
     private static List<String> _parseRawContents(String contents) {
-        final List<String> types = new ArrayList<>();
+        final List<String> rawPossibles = new ArrayList<>();
         // Whether we're currently parsing a regular expression, in a type
         boolean regex = false;
         // The type declaration, in this tag, that we're currently parsing
@@ -75,12 +98,12 @@ public class Tag {
                 currentType += c;
             } else {
                 // A '|' char means we've reached the end of a type (and a new one is going to start)
-                types.add(currentType);
+                rawPossibles.add(currentType);
                 currentType = "";
             }
         }
         // Add the last (or only) type
-        types.add(currentType);
-        return types;
+        rawPossibles.add(currentType);
+        return rawPossibles;
     }
 }
