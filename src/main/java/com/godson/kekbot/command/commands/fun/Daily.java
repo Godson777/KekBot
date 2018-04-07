49c6ed186a55bd6d897326c487ed4a548903bb1b
Godson <Godson777@users.noreply.github.com>
2018-04-06 23:42:20 -0400

    WEW DAILY TOPKEKS

---
+++ src/main/java/com/godson/kekbot/command/commands/fun/Daily.java
@@ -0,0 +1,39 @@
+package com.godson.kekbot.command.commands.fun;
+
+import com.godson.kekbot.CustomEmote;
+import com.godson.kekbot.KekBot;
+import com.godson.kekbot.Utils;
+import com.godson.kekbot.command.Command;
+import com.godson.kekbot.command.CommandEvent;
+import com.godson.kekbot.profile.Profile;
+
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+import java.util.Random;
+
+public class Daily extends Command {
+
+    private Random random = new Random();
+
+    public Daily() {
+        name = "daily";
+        description = "Claims your daily paycheck.";
+        usage.add("daily");
+        category = new Category("Fun");
+    }
+
+    @Override
+    public void onExecuted(CommandEvent event) {
+        Profile profile = Profile.getProfile(event.getAuthor());
+        if (profile.getDaily() != null && profile.getDaily().isAfter(event.getMessage().getCreationTime().toInstant())) {
+            event.getChannel().sendMessage("You've already claimed your daily today! Come back in: " + Utils.convertMillisToTime(event.getMessage().getCreationTime().toInstant().until(profile.getDaily(), ChronoUnit.MILLIS))).queue();
+            return;
+        }
+
+        profile.setDaily(event.getMessage().getCreationTime().plusDays(1));
+        int reward = random.nextInt(20);
+        profile.addTopKeks(reward);
+        profile.save();
+        event.getChannel().sendMessage("You're collected " + CustomEmote.printPrice(reward) + " today! Come back tomorrow for more!").queue();
+    }
+}
