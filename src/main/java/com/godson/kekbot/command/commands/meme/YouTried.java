8604563f7c70d7bcf02c5d385c66443f0a6fbdc4
Godson <Godson777@users.noreply.github.com>
2018-05-31 16:59:35 -0400

    And anotha meme command

---
+++ src/main/java/com/godson/kekbot/command/commands/meme/YouTried.java
@@ -0,0 +1,29 @@
+package com.godson.kekbot.command.commands.meme;
+
+import com.godson.kekbot.command.Command;
+import com.godson.kekbot.command.CommandCategories;
+import com.godson.kekbot.command.CommandEvent;
+
+import java.io.File;
+import java.util.Random;
+
+public class YouTried extends Command {
+
+    public YouTried() {
+        name = "youtried";
+        description = "Here's a gold star.";
+        usage.add("youtried");
+        category = CommandCategories.meme;
+        cooldownScope = CooldownScope.USER_GUILD;
+        cooldown = 5;
+    }
+
+    @Override
+    public void onExecuted(CommandEvent event) {
+        File stars[] = new File("resources/youtried").listFiles();
+        Random random = new Random();
+        int index = random.nextInt(stars.length);
+        event.getChannel().sendTyping().queue();
+        event.getChannel().sendFile(stars[index], stars[index].getName(), null).queue();
+    }
+}
