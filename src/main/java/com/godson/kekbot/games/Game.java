d18d7bb3b8b77456933592e5f33e3b623adfdfaf
Godson <Godson777@users.noreply.github.com>
2020-02-06 16:54:38 -0500

    incorrect math lol

--- src/main/java/com/godson/kekbot/games/Game.java
+++ src/main/java/com/godson/kekbot/games/Game.java
@@ -133,7 +133,7 @@ public abstract class Game {
                     if (topkeks > 0 && KXP > 0) builder.append(stateEarnings(winner, topkeks, KXP, (multiplier > 1 ? new Bonus(Precision.round(topkeks * (multiplier - 1), 2), multiplier + "x Multiplier") : null))).append("\n");
                 } else {
                     double betEarnings = bets.declareWinners(this, winnerIDs);
-                    profile.wonGame(Precision.round((topkeks + betEarnings) * multiplier, 2), KXP);
+                    profile.wonGame(Precision.round((topkeks * multiplier) + betEarnings, 2), KXP);
                     //if (bets.hasPlayerBets() || multiplier > 1) builder.append(stateEarnings(winner, topkeks, KXP, )).append("\n");
                     builder.append(stateEarnings(winner, topkeks * multiplier, KXP, (bets.hasPlayerBets() ? new Bonus(betEarnings, "Won Bet") : null), (multiplier > 1 ? new Bonus(Precision.round(topkeks * (multiplier - 1), 2), multiplier + "x Multiplier") : null))).append("\n");
                 }
@@ -173,7 +173,7 @@ public abstract class Game {
                     if (!(topkeks == 0 && KXP == 0)) builder.append(stateEarnings(winners.get(i), topkeks, KXP, (multiplier > 1 ? new Bonus(Precision.round(topkeks * (multiplier - 1), 2), multiplier + "x Multiplier") : null))).append("\n");
                 } else {
                     double betEarnings = bets.declareWinners(this, winnerIDs);
-                    profile.wonGame(Precision.round((baseTopkeks * multiplier) + (players.size() - i) + betEarnings, 2), KXP);
+                    profile.wonGame(Precision.round((topkeks * multiplier) + betEarnings, 2), KXP);
                     //if (bets.hasPlayerBets()) builder.append(stateEarnings(winners.get(i), topkeks, KXP, betEarnings, "Won Bet")).append("\n");
                     //else builder.append(stateEarnings(winners.get(i), topkeks, KXP)).append("\n");
                     builder.append(stateEarnings(winners.get(i), topkeks, KXP, (bets.hasPlayerBets() ? new Bonus(betEarnings, "Won Bet") : null), (multiplier > 1 ? new Bonus(Precision.round(topkeks * (multiplier - 1), 2), multiplier + "x Multiplier") : null))).append("\n");
